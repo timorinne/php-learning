14dfe3abceaf033da8e2a83ca05095102ed86b00
timorinne <timo.rinne@symbio.com>
2016-07-01 11:59:53 +0300

    Added more a for-each-loop

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'Testing my first PHP page!!!';
+echo 'Testing my first PHP page!!!<br>';
+
+$numbers = array(345,397,676,385,2657,5784);
+    
+foreach($numbers as $number){
+
+echo $number . "<br>";
+
+}    
 ?>
 
