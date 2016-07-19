c4965be1d393356ab03ac33b3ed2e9a8d2f678c2
timorinne <timo.rinne@symbio.com>
2016-07-20 00:22:53 +0300

    Merge origin/master
    Conflicts:
    	index.php

--- db-test.php
+++ db-test.php
@@ -7,3 +7,25 @@
  */
 
 echo "Hello everybody!";
+echo "Now we are try to connect to Azure SQL database[tri-php-study_db] on SQL Server[tri-azure-sqlserver.database.windows.net]";
+
+# 
+# Server: tri-azure-sqlserver.database.windows.net,1433 
+# SQL Database: tri-php-study_db\r\nUser 
+# Name: tri\r\n\r\nPHP Data Objects(PDO) 
+# 
+# Sample Code:
+# 
+try {
+    $conn = new PDO ('sqlsrv:server = tcp:tri-azure-sqlserver.database.windows.net,1433; Database = tri-php-study_db', 'tri', 'OhNoo8yu');
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} 
+catch ( PDOException $e ) {   
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
+
+echo "OK!";
+
+#   
+#   SQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"tri@tri-azure-sqlserver\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"tri-php-study_db\", \"LoginTimeout\" => 30, \"Encrypt\" => 1);\r\n$serverName = \"tcp:tri-azure-sqlserver.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
